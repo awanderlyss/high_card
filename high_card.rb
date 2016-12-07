50ae046e967af9139328ca5de7b00cb47f907966
awanderlyss <alyssa.felix7@gmail.com>
2016-12-07 10:31:02 -0500

    Add loop to collect player names

--- high_card.rb
+++ high_card.rb
@@ -1,25 +1,15 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
 deck = []
 collect = []
 
-
 # Build a deck of shuffled cards.
 #
 # The deck should be an array of cards.
 # Each card should be a mini-array with a value and suit. Example: [8, "hearts"]
-
-ranks.each do |rank|
-  suits.each do |suit|
-    collect.push(rank, suit)
-      deck.push(collect)
-        collect = []
-  end
-end
-
 # Shuffle the deck. It would look something like this...
 # deck = [
 #     [8, "hearts"],
@@ -28,12 +18,28 @@ end
 #     ...
 # ]
 
+ranks.each_with_index do |rank, val|
+  suits.each do |suit|
+    collect.push(rank, suit, val)
+    deck.push(collect)
+    collect = []
+  end
+end
+
 deck.shuffle
 
 # NOTE: do not hardcode the deck. Create it by performing methods on the two arrays in the starter code.
 # Collect an array of player names by prompting:
 
 # "{n} players so far. Enter a player name, or type 'play':" Should look like this: [{name:'Jesse'}, {name:'Jane'}]
+loop do
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
+  name = gets.chomp
+  break if name == 'play'
+  players << [{name: name}]
+end
+
+puts players
 # Upon "play", deal each player a card.
 # Find the highest card score dealt (Aces high).
 # Find the winning player name, then print out:
